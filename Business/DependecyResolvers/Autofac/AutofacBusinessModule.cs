fece5499f0c630ff4fd60cb1ae30a328da15f653
furkan sakızcı <fursakizci@hotmail.com>
2021-07-27 16:09:14 +0300

    Autofac configurations

---
+++ Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependecyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //buradaki kodun karsiligi startup icerisinde singleton dir.
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
